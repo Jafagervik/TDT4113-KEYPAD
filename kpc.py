57ad9f3c9bd03914d219a7014f9853b1c627313d
Jørgen Aleksander Fagervik <jorgenafagervik@gmail.com>
2019-10-09 09:52:01 +0200

    Added skeleton methods for FSM <--> Agent interaction

--- kpc.py
+++ kpc.py
@@ -32,3 +32,28 @@ class KPC:
     def exit_action(self):
         # shutdown method - power down lightning sequence
         pass
+
+    # Rule Methods For FSM
+    def reset_password_accumulator(self):
+        pass
+
+    def append_next_password_digit(self):
+        pass
+
+    def reset_agent(self):
+        pass
+
+    def verify_password(self):
+        pass
+
+    def cache_first_new_password(self):
+        pass
+
+    def compare_new_password_digit(self, password):
+        with open('password.txt', "r") as f:
+            pwd = f.read()
+            for line in pwd:
+                for char in range(len(line)):
+                    if line[char] != password[char]:
+                        return False
+            return True
